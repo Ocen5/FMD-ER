cf910462a30e05bb5460024598e01b0e37262664
TheBlakeOmega <48212564+TheBlakeOmega@users.noreply.github.com>
2020-07-13 15:29:49 +0200

    final version of CNN

---
+++ face_network.py
@@ -0,0 +1,130 @@
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
+import random
+from keras.optimizers import Adam
+from keras.applications.vgg16 import preprocess_input
+from keras.callbacks import ModelCheckpoint
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.regularizers import l2
+from keras_vggface.vggface import VGGFace
+from keras.engine import Model
+
+IMAGE_SIZE = 48
+random.seed(3)
+num_features = 64
+num_labels = 7
+
+#Preprocessing dei dati
+train_folder = "train/"
+val_folder = "validation/"
+
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+train_batchsize = 34
+val_batchsize = 8
+
+train_generator = train_datagen.flow_from_directory(
+        train_folder,
+        target_size=(IMAGE_SIZE, IMAGE_SIZE),
+        batch_size=train_batchsize,
+        class_mode="categorical"
+)
+
+val_generator = val_datagen.flow_from_directory(
+        val_folder,
+        target_size=(IMAGE_SIZE, IMAGE_SIZE),
+        batch_size=val_batchsize,
+        class_mode="categorical",
+        shuffle=False
+)
+
+
+# Addestramento rete
+model = Sequential()
+
+model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3), data_format='channels_last', kernel_regularizer=l2(0.01)))
+model.add(Conv2D(num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(Conv2D(2*2*2*num_features, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+
+model.add(Dense(2*2*2*num_features, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*2*num_features, activation='relu'))
+model.add(Dropout(0.4))
+model.add(Dense(2*num_features, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(num_labels, activation='softmax'))
+
+filepath = "/vfaceModel.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+model.compile(loss="categorical_crossentropy",
+              optimizer=Adam(lr=1.25e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-7),
+              metrics=['accuracy'])
+
+print("Compile success... Start Train")
+
+history = model.fit_generator(
+        train_generator,
+        epochs=50,
+        validation_data=val_generator,
+        verbose=1,
+        callbacks=callbacks_list
+    )
+
+print("Saved model to disk")
+model.save("cropped_face.h5")
+
+# Risultato addestramento
+acc = history.history["accuracy"]
+val_acc = history.history["val_accuracy"]
+loss = history.history["loss"]
+val_loss = history.history["val_loss"]
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'b', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.savefig("Accuracy cropped eyes")
+
+plt.figure()
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.savefig("Loss cropped eyes")
